5ca4b1b002fdf6accafbaa5869ffe83c3f4e31d7
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2025-02-27 17:09:33 -0300

    routes configuration

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonConatiner, ButtonVariant } from './Button.styles'
-
-interface ButtonProps {
-  variant?: ButtonVariant
-}
-
-export function Button({ variant = 'primary' }: ButtonProps) {
-  return <ButtonConatiner variant={variant}>Click me</ButtonConatiner>
-}
