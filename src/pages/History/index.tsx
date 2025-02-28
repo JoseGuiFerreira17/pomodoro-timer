bd46f87e194b3b43f7d9a3ac945fde7c8e63b798
JoseGuiFerreira17 <joseguiferreira17@gmail.com>
2025-02-28 15:21:39 -0300

    history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from './styles'
+import { HistoryContainer, HistoryList, Status } from './styles'
 
 export function History() {
   return (
@@ -19,37 +19,25 @@ export function History() {
               <td>Estudar React</td>
               <td>20 minutos</td>
               <td>Há cerca de 2 horas</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="green">Concluído</Status>
+              </td>
             </tr>
             <tr>
               <td>Estudar React</td>
               <td>20 minutos</td>
               <td>Há cerca de 2 horas</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
             </tr>
             <tr>
               <td>Estudar React</td>
               <td>20 minutos</td>
               <td>Há cerca de 2 horas</td>
-              <td>Concluído</td>
-            </tr>
-            <tr>
-              <td>Estudar React</td>
-              <td>20 minutos</td>
-              <td>Há cerca de 2 horas</td>
-              <td>Concluído</td>
-            </tr>
-            <tr>
-              <td>Estudar React</td>
-              <td>20 minutos</td>
-              <td>Há cerca de 2 horas</td>
-              <td>Concluído</td>
-            </tr>
-            <tr>
-              <td>Estudar React</td>
-              <td>20 minutos</td>
-              <td>Há cerca de 2 horas</td>
-              <td>Concluído</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
             </tr>
           </tbody>
         </table>
